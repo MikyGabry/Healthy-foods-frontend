9c422d713ba9dbb3b6ecc8c0b845cc631b28e0e7
Miky.Gabry <isgromichael@gmail.com>
2023-05-23 22:33:04 -0400

    Create the homePage and linked with the API

---
+++ src/pages/Home.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react"
+
+function Home () {
+
+    const [foods, setFoods] = useState(null)
+    
+    async function  fetchFoods() {
+        try{
+            let myFoods = await fetch('https://healthy-foods-s1ri.onrender.com/food/')
+            myFoods = await myFoods.json()
+            setFoods(myFoods)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() =>{
+        fetchFoods();
+    }, [])
+
+    function randomDish() {
+        let featuredDish = 0;
+        if (foods) {
+            featuredDish = Math.floor(Math.random() * (foods.length))
+            console.log(featuredDish)
+            return (
+                <>
+                <h2>{foods[featuredDish].dishName}</h2>
+                <h3>{foods[featuredDish].cuisine}</h3>
+                <h3>{foods[featuredDish].ingredients}</h3>
+                <h3>{foods[featuredDish].recipe}</h3>
+                </>
+            )
+        } else <h2>Loading...</h2>
+
+    }
+    console.log(foods)
+    
+    return(
+        <>
+        {randomDish()}
+        </>
+    )
+}
+
+export default Home;
