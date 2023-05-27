3794859c99b5c59ee1a4582d712ae961936124b5
Miky.Gabry <isgromichael@gmail.com>
2023-05-27 16:35:18 -0400

    Create the FoodsIndex pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './pages/Home';
 import Header from './components/Header';
+import FoodsIndex from './pages/FoodsIndex';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/food' element={<FoodsIndex />} />
       </Routes>
       {/* <Home /> */}
     </>
