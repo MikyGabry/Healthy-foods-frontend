95b2edf48e429c0f5b17c24f00497ca52954c067
Miky.Gabry <isgromichael@gmail.com>
2023-05-29 18:00:40 -0400

    Create the FoodsShow page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './pages/Home';
 import Header from './components/Header';
 import FoodsIndex from './pages/FoodsIndex';
+import FoodsShow from './pages/FoodsShow';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -10,7 +11,10 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/food' element={<FoodsIndex />} />
+        <Route path='/food'>
+          <Route path='' element={<FoodsIndex />} />
+          <Route path=':foodId' element={<FoodsShow />} />
+        </Route>
       </Routes>
       {/* <Home /> */}
     </>
